82fc97c4d1272dd51f6ad95a5c79a71611b1043a
thoth <xolotl15@gmail.com>
2022-03-25 20:20:23 -0500

    Patching input errors

--- game_computer.py
+++ game_computer.py
@@ -2,28 +2,34 @@ from random import randint as rndi
 from time import sleep as slp
 
 def game():
-    number : int = int ( input('Enter a number from 1 to 100, for the computer to guess: ') )
-    # print (number)
+    while True:
+        try:
+            number : int = int ( input('Enter a integer number from 1 to 100, for the computer to guess: ') )
+            break
+        except ValueError:
+            print ('Incorrect value, try again')
     
     initial = 1
     final = 100
     guess_number : int = rndi(initial,final)
     print (guess_number)
-    
+    count = 1
+
     while True:
         if number == guess_number:
-            print (f'You guessed the number ({number}), Congratulations!')
+            print (f'Computer guesses the number ({number}) in {count} attemps')
             break
         elif number < guess_number:
-            print ('The correct number is lower, try again...')
+            print ('The correct number is lower, try again buddy...')
             final = guess_number - 1
             guess_number : int = rndi(initial,final)
         else:
-            print ('The correct number is greater, try again...')
+            print ('The correct number is greater, try again buddy...')
             initial = guess_number + 1
             guess_number : int = rndi(initial,final)
         print (guess_number)
         slp(1)
+        count += 1
 
 
 if __name__ == '__main__':
--- game_user.py
+++ game_user.py
@@ -3,17 +3,26 @@ from time import sleep as slp
 
 def game():
     number: int = rndi(1,100)
-        
+    count = 1
+
     while True:
-        guess_number: int = int( input('Enter a number from 1 to 100: ') )
+        while True:
+            try:
+                guess_number: int = int( input('Enter a number from 1 to 100: \n>>>>> ') )
+                break
+            except ValueError:
+                print ('Error in value, try again...')
+                slp(0.5)
+
         if number == guess_number:
-            print (f'You guessed the number ({number}), Congratulations!')
+            print (f'You guessed the number ({number}) in just {count} attemps, Congratulations!')
             break
         elif number < guess_number:
             print ('The correct number is lower, try again...')
         else:
             print ('The correct number is greater, try again...')
         slp(1)
+        count +=1
 
 
 if __name__ == '__main__':
